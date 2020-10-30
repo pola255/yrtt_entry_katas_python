7e79e140e84ebdf9da89370d432d47cfc09d30e2
Paula <paulette255@gmail.com>
2020-10-30 16:55:08 +0000

    solutions

--- tasks/exercise003.py
+++ tasks/exercise003.py
@@ -1,8 +1,12 @@
 # Scenario
-# Several people are standing in a row divided into two teams. The first person goes into team 1, the second goes into team 2, the third goes into team 1, and so on.
+# Several people are standing in a row divided into two teams. The first
+# person goes into team 1, the second goes into team 2, the third goes
+# into team 1, and so on.
 
 # Task
-# Given an array of positive integers (the weights of the people), return a new array of two integers, where the first one is the total weight of team 1, and the second one is the total weight of team 2.
+# Given an array of positive integers (the weights of the people), return
+# a new array of two integers, where the first one is the total weight of
+# team 1, and the second one is the total weight of team 2.
 
 # Notes
 # Array size is at least 1.
@@ -11,28 +15,26 @@
 # Input >> Output Examples
 # rowWeights([13, 27, 49])  ==>  return (62, 27)
 # Explanation:
-# The first element 62 is the total weight of team 1, and the second element 27 is the total weight of team 2.
+# The first element 62 is the total weight of team 1, and the second
+# element 27 is the total weight of team 2.
 
 # rowWeights([50, 60, 70, 80])  ==>  return (120, 140)
 # Explanation:
-# The first element 120 is the total weight of team 1, and the second element 140 is the total weight of team 2.
+# The first element 120 is the total weight of team 1, and the second
+# element 140 is the total weight of team 2.
 
 # rowWeights([80])  ==>  return (80, 0)
 # Explanation:
-# The first element 80 is the total weight of team 1, and the second element 0 is the total weight of team 2.
+# The first element 80 is the total weight of team 1, and the second
+# element 0 is the total weight of team 2.
 
 def row_weights(array):
-    if len(array) > 0:
-        if len(array) == 1:
-            return [array[0], 0]
+    sum_weight_team_1 = 0
+    sum_weight_team_2 = 0
+    for i, weight in enumerate(array):
+        if i == 0 or i % 2 == 0:
+            sum_weight_team_1 = sum_weight_team_1 + weight
         else:
-            sum_weight_team_1 = 0
-            sum_weight_team_2 = 0
-            for i, weight in enumerate(array):
-                if i == 0 or i % 2 == 0:
-                    sum_weight_team_1 = sum_weight_team_1 + weight
-                else:
-                    sum_weight_team_2 = sum_weight_team_2 + weight
-        
-            return [sum_weight_team_1, sum_weight_team_2]
-    
+            sum_weight_team_2 = sum_weight_team_2 + weight
+
+    return [sum_weight_team_1, sum_weight_team_2]
--- tasks/exercise004.py
+++ tasks/exercise004.py
@@ -1,4 +1,4 @@
-# Move the first letter of each word to the end of it, then add "ay" 
+# Move the first letter of each word to the end of it, then add "ay"
 # to the end of the word. Leave punctuation marks untouched.
 
 def pig_it(text):
@@ -10,12 +10,13 @@ def pig_it(text):
         n = len(word) - 1
         while n > 0:
             if word[n].isalpha():
-                new_word = word[1:n+1] + word[0] + 'ay'
+                new_word = word[1:n + 1] + word[0] + 'ay'
                 n = 0
             else:
                 final_characteres = final_characteres + word[n]
             n -= 1
         new_word = new_word + final_characteres
+        final_characteres = ''
         if new_text == '':
             new_text = new_word
         else:
